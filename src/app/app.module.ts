2a5b1ec98d4568cc1fbbf53ad440269fa2d0e9eb
Jhon Castillo <wooody0313@gmail.com>
2019-01-24 16:22:36 -0500

    redux sample

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { ReactiveFormsModule} from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodosListComponent } from './todo/todos-list/todos-list.component';
+import { TodoItemComponent } from './todo/todo-item/todo-item.component';
+import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
+import { TodoAddComponent } from './todo/todo-add/todo-add.component';
+
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { appReducers } from './app.reducers';
+
+import { environment } from 'src/environments/environment.prod';
+import { FilterPipe } from './filter/filter.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    TodoComponent,
+    TodosListComponent,
+    TodoItemComponent,
+    TodoFooterComponent,
+    TodoAddComponent,
+    FilterPipe
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    [StoreModule.forRoot(appReducers)],
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
---
+++ src/app/todo/todo-add/todo-add.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+import { AppState } from 'src/app/app.reducers';
+
+import { Store } from '@ngrx/store';
+
+import * as fromTodo from '../todo.actions'
+
+@Component({
+  selector: 'app-todo-add',
+  templateUrl: './todo-add.component.html',
+  styles: []
+})
+export class TodoAddComponent implements OnInit {
+
+  txtInput: FormControl;
+
+  constructor(private store: Store<AppState>) { }
+
+  ngOnInit() {
+    this.txtInput = new FormControl('', Validators.required);
+  }
+
+  agregarTodo() {
+    if (!this.txtInput.valid) {
+      return;
+    }
+
+    const accion = new fromTodo.AgregarTodoAction(this.txtInput.value);
+    this.store.dispatch(accion);
+
+    this.txtInput.setValue('');
+
+  }
+
+}
